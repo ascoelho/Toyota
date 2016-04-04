a3e1b5682395df1579b6c23cac73da95dfa6c550
ascoelho <anthonystefancoelho@gmail.com>
2016-04-04 13:52:46 -0400

    Initial Commit

---
+++ Toyota/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Toyota
+//
+//  Created by Anthony Coelho on 2016-04-04.
+//  Copyright © 2016 Vectura. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
